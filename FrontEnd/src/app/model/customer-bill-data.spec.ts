0c18258a5d0cbf6e0359697ae6581db824ce16a2
Saravana kumar <66402906+saravanasathie@users.noreply.github.com>
2023-09-27 05:44:48 +0530

    Delete FrontEnd directory

--- FrontEnd/src/app/model/customer-bill-data.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CustomerBillData } from './customer-bill-data';
-
-describe('CustomerBillData', () => {
-  it('should create an instance', () => {
-    expect(new CustomerBillData()).toBeTruthy();
-  });
-});
--- FrontEnd/src/app/model/invoice.ts
+++
@@ -1,7 +0,0 @@
-export interface Invoice {
-    invoiceId:number;
-	beforeDueDateAmout:number;
-	afterDueDateAmout:number;
-	billId:number;
-	ratePerKW:number;
-}
